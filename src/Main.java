1e920e1ab41808ef91554d679052af6a85a0788a
Vadim <fishermanvadik@gmail.com>
2023-04-17 17:24:39 +0300

    Zalil dz

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = IntStream.generate(() -> ThreadLocalRandom.current().nextInt(10)).limit(10).toArray();
+        System.out.println("Массив до сортировки:\n" + Arrays.toString(array));
+        System.out.println();
+        countSort(array);
+    }
+
+    private static void countSort(int[] array) {
+        Map<Integer, Integer> listPairs = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int count = 1;
+            if (checkElement(array, array[i], i)) {
+                continue;
+            }
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] == array[i]) {
+                    count++;
+                }
+            }
+            listPairs.put(array[i], count);
+        }
+        ArrayList<Integer> listValues = new ArrayList<>();
+        ArrayList<Integer> listCounts = new ArrayList<>();
+        ArrayList<Integer> sortedArray = new ArrayList<>();
+        for (int key : listPairs.keySet()) {
+            listValues.add(key);
+        }
+        for (int value : listPairs.values()) {
+            listCounts.add(value);
+        }
+        for (int i = 0; i < listValues.size(); i++) {
+            int countdown = listCounts.get(i);
+            while (countdown > 0) {
+                sortedArray.add(listValues.get(i));
+                countdown--;
+            }
+        }
+        System.out.println("Массив после сортировки:\n" + sortedArray);
+
+    }
+
+    private static boolean checkElement(int[] array, int element, int endIndex) {
+        boolean check = false;
+        for (int i = 0; i < endIndex; i++) {
+            if (array[i] == element) {
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+}
